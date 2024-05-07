8bd43f831e2ebdcceb0c58c4bb58be5bfd826a5b
Pedro Vinicius <89553913+PedroViniciussd@users.noreply.github.com>
2024-05-07 23:43:46 +0000

    Criação do HTML e CSS para o Card

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import './App.css'
 function App() {
   return (
     <>
-      App
+      <div className='card'> 
+        <h1>Who is this pokemon?</h1>
+        <img src="https://www.salvatore.academy/devmon/2_kotlin.png" width="200" />
+      </div>
     </>
   )
 }
