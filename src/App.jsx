7f2d547f6c2392ee5e6ed62da772d4d15222c735
Pedro Vinicius <89553913+PedroViniciussd@users.noreply.github.com>
2024-05-08 21:57:54 +0000

    Criação do Component 'Card'

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import './App.css'
+import Card from './components/Card/Card'
 
 function App() {
   return (
     <>
-      <div className='card'> 
-        <h1>Who is this pokemon?</h1>
-        <img src="https://www.salvatore.academy/devmon/2_kotlin.png" width="200" />
-      </div>
+      <Card />
+      <Card />
     </>
   )
 }
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,6 @@
+export default function Card() {
+    return <div className='card'> 
+    <h1>Who is this pokemon?</h1>
+    <img src="https://www.salvatore.academy/devmon/2_kotlin.png" width="200" />
+  </div>
+}
